4ccdd9e0753394e5f7ff0a57b1ddc1067bfbb01a
siro <hch4102@naver.com>
2021-06-06 19:49:48 +0900

    feat: Vue-router + Vuetify
    Vue-router + Vuetify 공부중
    
    Signed-off-by: siro <hch4102@naver.com>

---
+++ vue-router/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import Vuetify from "vuetify";
+
+export default {
+  name   : 'App',
+  vuetify: new Vuetify(),
+  props  : {
+    source: String,
+  },
+  data   : () => ({
+    drawer: null,
+  }),
+};
+</script>
---
+++ vue-router/src/router/router.js
@@ -0,0 +1,25 @@
+import Vue       from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path     : '/',
+    name     : 'home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path     : '/login',
+    name     : 'credential',
+    component: () => import('../views/Credential.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
